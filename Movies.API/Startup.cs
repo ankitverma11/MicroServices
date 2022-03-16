fdf692655c2d33437b39431cd9ade835273d400c
ankitmac <anki.verma02@gmail.com>
2022-03-16 17:30:01 +0530

    Add new Model (Movie) , New Movie Controller via scaffolding approach , New DB context class MovieAPIContext, Update in startup class to registe the service db context , also update in app setting to add connection string for db context

--- Movies.API/Startup.cs
+++ Movies.API/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Movies.API.Data;
 
 namespace Movies.API
 {
@@ -26,6 +28,9 @@ namespace Movies.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<MoviesAPIContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("MoviesAPIContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Movies.API/WeatherForecast.cs
+++ Movies.API/WeatherForecast.cs
@@ -9,7 +9,7 @@ namespace Movies.API
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
+        
         public string Summary { get; set; }
 
         public string CurrentWeather { get; set; }
