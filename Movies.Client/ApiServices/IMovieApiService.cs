ae793bcf31af7adef2f521aa67aa236a6d5f5b78
ankitmac <anki.verma02@gmail.com>
2022-03-30 12:22:00 +0530

    Add new client Project for featching data from API , currently create a API Service for getting Mock data

---
+++ Movies.Client/ApiServices/IMovieApiService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+    public interface IMovieApiService
+    {
+        Task<IEnumerable<Movie>> GetMovies();
+        Task<Movie> GetMovie(int id);
+        Task<Movie> CreateMovie(Movie movie);
+        Task<Movie> UpdateMovie(Movie movie);
+        Task DeleteMovie(int id);
+    }
+}
---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+             throw new  NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var moviesList = new List<Movie>();
+            moviesList.Add(
+                new Movie
+                {
+                    Id = 1,
+                    Genre = "Drama",
+                    Title = "The ShawShank Redemption",
+                    Rating = "9.3",
+                    ImageURL = "image/src",
+                    ReleaseDate = new DateTime(1994, 5, 5),
+                    Owner = "Alice"
+
+                });
+
+            return await Task.FromResult(moviesList);
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
